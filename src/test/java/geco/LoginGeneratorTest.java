dd2c60539f16c9d6f34b556566a6b5c971eab5fe
Rialy <rialy.andriamiseza@yahoo.fr>
2017-11-23 11:33:18 +0100

    testLoginGenerator

--- src/test/java/geco/LoginGeneratorTest.java
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -2,22 +2,36 @@ package geco;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class LoginGeneratorTest {
 
-    String nomL = "Tureau",
-        prenomL = "Laëtitia",
-        nomR = "Andriamiseza",
-        prenomR = "Rialy";
+    String nomL = "accent",
+        prenomL = "éxemple";
+    String[] logins = new String[0];
+    LoginService l = new LoginService(logins);
 
-    /* Initialisation du login generator
-    LoginGenerator lTest = new LoginGenerator()
+    /* Initialisation du login generator */
+    LoginGenerator lTest = new LoginGenerator(l);
 
     @Test
     public void testGenerateLoginForNomAndPrenom() throws Exception {
-        String sTest = LoginGenerator.generateLoginForNomAndPrenom(nomL, prenomL);
-    }*/
+        String sTest = lTest.generateLoginForNomAndPrenom(nomL, prenomL);
+        assertThat(sTest, is("EACC"));
+    }
 
+    @Test
+    public void testGenerateLoginForNomAndPrenom2() throws Exception {
+        l.addLogin("EACC");
+        String sTest = lTest.generateLoginForNomAndPrenom(nomL, prenomL);
+        assertThat(sTest, is("EACC1"));
+    }
 
+    @Test
+    public void testPaulDurand() throws Exception {
+        l = new LoginService(new String[]{"JROL", "BPER", "CGUR",  "JDUP", "JRAL", "JRAL1"});
+        String sTest = lTest.generateLoginForNomAndPrenom("Durand", "Paul");
+        assertThat(sTest, is("PDUR"));
+    }
 }
